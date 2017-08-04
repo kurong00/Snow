040cacf5e398c566ddd907863dc8e1ba1403cea0
kurong00 <kurong00@gmail.com>
2017-08-04 16:42:24 +0800

    add snow control script

---
+++ Snow/Assets/Script/SnowControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowControl : MonoBehaviour {
+    
+    GameObject rocks;
+	void Start () {
+        if(gameObject.tag== "BackGround")
+        {
+            gameObject.GetComponent<Renderer>().
+                sharedMaterial.SetFloat("_SnowRank", 0.00f);
+        }
+        else if(gameObject.GetComponent<Renderer>() != null)
+            gameObject.GetComponent<Renderer>().
+                sharedMaterial.SetFloat("_SnowRank", 0.0f);
+    }
+	void Update () {
+		
+	}
+}
