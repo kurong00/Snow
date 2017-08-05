cfae4d13fefeb568426781220ddd30f9e41b8ba3
kurong00 <kurong00@gmail.com>
2017-08-05 17:46:45 +0800

    done

---
+++ Snow/Assets/Script/Snow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Snow : MonoBehaviour {
+    
+    Renderer snowRenderer;
+    SnowControl snowControl;
+    float time = 0.0f;
+	void Start () {
+        snowControl = GameObject.Find("SnowControl").GetComponent<SnowControl>();
+        snowRenderer = gameObject.GetComponent<Renderer>();
+        if (gameObject.GetComponent<Renderer>() != null)
+            snowRenderer.sharedMaterial.SetFloat("_SnowRank", 0.0f);
+    }
+	void Update () {
+        Time.timeScale = snowControl.time + 1;
+        time += Time.deltaTime + snowControl.time / 50;
+        if (gameObject.tag == "BackGround")
+        {
+            snowRenderer.sharedMaterial.SetFloat("_SnowRank", time / 8000);
+        }
+        else if (gameObject.GetComponent<Renderer>() != null)
+            snowRenderer.sharedMaterial.SetFloat("_SnowRank", time / 500);
+    }
+}
--- Snow/Assets/Script/SnowControl.cs
+++ Snow/Assets/Script/SnowControl.cs
@@ -1,34 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SnowControl : MonoBehaviour {
-    
-    Renderer snowRenderer;
-    float time = 0.0f;
-	void Start () {
-        snowRenderer = gameObject.GetComponent<Renderer>();
-        if (gameObject.GetComponent<Renderer>() != null)
-            snowRenderer.sharedMaterial.SetFloat("_SnowRank", 0.0f);
-    }
-	void Update () {
-        time += Time.deltaTime;
-        if (gameObject.tag == "BackGround")
-        {
-            snowRenderer.sharedMaterial.SetFloat("_SnowRank", time / 8000);
-        }
-        else if (gameObject.GetComponent<Renderer>() != null)
-            snowRenderer.sharedMaterial.SetFloat("_SnowRank", time / 500);
-    }
+
+    public Slider timeRank;
+    [HideInInspector]
+    public float time = 0.0f;
 
     public void ClearSnow()
     {
         SceneManager.LoadScene("Main");
     }
 
-    public void AddSnowRank()
+    public void UpdateTime()
     {
-        Time.timeScale = 2;
+        time = timeRank.value;
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
 }
